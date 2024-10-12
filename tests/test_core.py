2a9797df0962a4320b11330db6247f999c6e656e
ftnext <takuyafjp+develop@gmail.com>
2024-10-12 12:53:35 +0900

    [refactor] Introduce class

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,26 +5,27 @@ from unittest.mock import ANY
 from kotoha.core import ArgumentConcreteTypeHintChecker
 
 
-def test_ArgumentConcreteTypeHintChecker() -> None:
-    code = dedent(
-        """\
-    from collections.abc import Iterable
+class TestArgumentConcreteTypeHintChecker:
+    def test_KTH000(self):
+        code = dedent(
+            """\
+        from collections.abc import Iterable
 
-    print("Hello, world!")
-
-
-    def plus_one_ng(numbers: list[int]) -> list[int]:
-        return [n + 1 for n in numbers]
+        print("Hello, world!")
 
 
-    def plus_one_ok(numbers: Iterable[int]) -> list[int]:
-        return [n + 1 for n in numbers]
-    """
-    )
+        def plus_one_ng(numbers: list[int]) -> list[int]:
+            return [n + 1 for n in numbers]
 
-    checker = ArgumentConcreteTypeHintChecker()
-    checker.visit(ast.parse(code))
 
-    assert len(checker.errors) == 1
-    assert checker.errors[0] == (6, 16, ANY)
-    assert checker.errors[0][2].startswith("KTH000")
+        def plus_one_ok(numbers: Iterable[int]) -> list[int]:
+            return [n + 1 for n in numbers]
+        """
+        )
+
+        checker = ArgumentConcreteTypeHintChecker()
+        checker.visit(ast.parse(code))
+
+        assert len(checker.errors) == 1
+        assert checker.errors[0] == (6, 16, ANY)
+        assert checker.errors[0][2].startswith("KTH000")
